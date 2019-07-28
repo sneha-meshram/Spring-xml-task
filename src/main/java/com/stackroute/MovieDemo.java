27cc416cefa63b08c3a45071376fdd1a2d8c9632
sneha <sneha.meshram@cgi.com>
2019-07-28 18:49:48 +0530

    auto-wire xml task committed

--- src/main/java/com/stackroute/MovieDemo.java
+++ src/main/java/com/stackroute/MovieDemo.java
@@ -7,6 +7,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
+/*
+For the Movie bean, delete the constructor based object injection in the bean definition file
+(beans.xml) that injects an Actor bean.
+Use autowire byName in the Movie bean to inject an Actor bean.
+Run the application.
+Create another Movie bean and try autowire byType.
+Run the application and note the exception thrown.
+Fix the Movie bean by removing autowire byType and using constructor injection instead.
+
+ */
+
+
 public class MovieDemo {
 
     public static void main(String[] args){
@@ -18,7 +30,7 @@ public class MovieDemo {
 
         Movie movie2=context.getBean("movie1",Movie.class);
         System.out.println(movie2.getActors());
-        System.out.println(movie1==movie2);
+        System.out.println((movie1==movie2));
 
 //        //intance of interface IOC class
 //        XmlBeanFactory fac= new XmlBeanFactory(new ClassPathResource("beans.xml"));
